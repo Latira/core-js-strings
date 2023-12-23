64e73f72f603952d8215791fba274a4ac6b75bee
Latira <l@g.com>
2023-12-23 08:12:44 +0300

    update: new solutions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -221,11 +221,11 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  // if ( ) {
-
-  // }
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  if (str.startsWith(substr)) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -239,8 +239,11 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  if (str.endsWith(substr)) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -256,8 +259,10 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const mins = String(minutes).padStart(2, '0');
+  const secs = String(seconds).padStart(2, '0');
+  return `${mins}:${secs}`;
 }
 
 /**
